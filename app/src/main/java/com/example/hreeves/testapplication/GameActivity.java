efc30b146c64e1cd8d56d04b7c9aab352e6d617a
dgray <dgray@islander.tamucc.edu>
2017-04-22 12:37:46 -0500

    Working on Game Activity features

--- app/src/main/java/com/example/hreeves/testapplication/GameActivity.java
+++ app/src/main/java/com/example/hreeves/testapplication/GameActivity.java
@@ -2,12 +2,28 @@ package com.example.hreeves.testapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class GameActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        System.out.println("Game Activity activated");
+
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_game);
+        //Set to full screen
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        //turn title off
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        setContentView(new GamePanel(this));
     }
+
+
+
+
+
 }
